9eb2dcf798a837375b178805a52b7cd0dbd28f68
Hifi58 <gillet.tony@gmail.com>
2021-09-13 13:57:57 +0200

    structure des sections

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
+//Variables de sections
+
 let main = document.getElementById('main');
 let whalesection = document.getElementById('whale-section');
+
+//Variables de boutons
 let whale = document.getElementById('whale');
 
 whale.addEventListener('click', function(){
