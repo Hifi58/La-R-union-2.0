f0b360fb95aba9a96c51ea527d949ce11f8c5415
Hifi58 <gillet.tony@gmail.com>
2021-09-13 15:02:13 +0200

    fonction switch fonctionnelle

--- js/script.js
+++ js/script.js
@@ -2,11 +2,25 @@
 
 let main = document.getElementById('main');
 let whalesection = document.getElementById('whale-section');
+let landscapesection = document.getElementById('landscape-section');
+
+let foodsection = document.getElementById('food-section');
 
 //Variables de boutons
 let whale = document.getElementById('whale');
+let landscape = document.getElementById('landscape');
 
-whale.addEventListener('click', function(){
-     main.style.display="none";
-     whalesection.style.display="block";
-})
+
+//Listener dot
+
+function switchWhale(){
+    main.style.display = (main.style.display == 'none' ? '' : 'none');
+    whalesection.style.display = (whalesection.style.display == 'block' ? '' : 'block');
+}
+
+function switchMain1(){
+    whalesection.style.display = (main.style.display == 'none' ? '' : 'none');
+    main.style.display = (whalesection.style.display == 'block' ? '' : 'block');
+}
+
+
