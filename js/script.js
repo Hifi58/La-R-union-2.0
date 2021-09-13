96f387b7c035493127d63d4552df11fbc9769583
Hifi58 <gillet.tony@gmail.com>
2021-09-13 15:48:59 +0200

    maj

--- js/script.js
+++ js/script.js
@@ -1,26 +1,98 @@
 //Variables de sections
 
-let main = document.getElementById('main');
-let whalesection = document.getElementById('whale-section');
-let landscapesection = document.getElementById('landscape-section');
+
+
 
 let foodsection = document.getElementById('food-section');
 
-//Variables de boutons
-let whale = document.getElementById('whale');
-let landscape = document.getElementById('landscape');
+//Switch Main
+function switchMain1(){
+    whalesection.style.display = (main.style.display == 'none' ? '' : 'none');
+    main.style.display = (whalesection.style.display == 'block' ? '' : 'block');
+}
 
+function switchMain2(){
+    landscapesection.style.display = (main.style.display == 'none' ? '' : 'none');
+    main.style.display = (whalesection.style.display == 'block' ? '' : 'block');
+}
 
-//Listener dot
+function switchMain3(){
+    seasection.style.display = (main.style.display == 'none' ? '' : 'none');
+    main.style.display = (whalesection.style.display == 'block' ? '' : 'block');
+}
+
+//Switch Whale
+let main = document.getElementById('main');
+let whalesection = document.getElementById('whale-section');
 
 function switchWhale(){
     main.style.display = (main.style.display == 'none' ? '' : 'none');
     whalesection.style.display = (whalesection.style.display == 'block' ? '' : 'block');
 }
 
-function switchMain1(){
+function switchWhaleLandscape(){
     whalesection.style.display = (main.style.display == 'none' ? '' : 'none');
-    main.style.display = (whalesection.style.display == 'block' ? '' : 'block');
+    landscapesection.style.display = (whalesection.style.display == 'block' ? '' : 'block');
+}
+
+function switchWhaleSea(){
+    whalesection.style.display = (main.style.display == 'none' ? '' : 'none');
+    seasection.style.display = (whalesection.style.display == 'block' ? '' : 'block');
 }
 
 
+
+
+//Switch Landscape
+let landscapesection = document.getElementById('landscape-section');
+
+function switchLandscape(){
+    main.style.display = (main.style.display == 'none' ? '' : 'none');
+    landscapesection.style.display = (whalesection.style.display == 'block' ? '' : 'block');
+}
+
+function switchLandscapeWhale(){
+    landscapesection.style.display = (main.style.display == 'none' ? '' : 'none');
+    whalesection.style.display = (whalesection.style.display == 'block' ? '' : 'block');
+}
+
+function switchLandscapeSea(){
+    landscapesection.style.display = (main.style.display == 'none' ? '' : 'none');
+    seasection.style.display = (whalesection.style.display == 'block' ? '' : 'block');
+}
+
+//Switch sea
+let seasection = document.getElementById('sea-section');
+
+function switchSea(){
+    main.style.display = (main.style.display == 'none' ? '' : 'none');
+    seasection.style.display = (whalesection.style.display == 'block' ? '' : 'block');
+}
+
+function switchSeaWhale(){
+    seasection.style.display = (main.style.display == 'none' ? '' : 'none');
+    whalesection.style.display = (whalesection.style.display == 'block' ? '' : 'block');
+}
+
+function switchSeaLandscape(){
+    seasection.style.display = (main.style.display == 'none' ? '' : 'none');
+    landscapesection.style.display = (whalesection.style.display == 'block' ? '' : 'block');
+}
+
+//Switch bird
+let birdsection = document.getElementById('birds-section');
+
+function switchBird(){
+    main.style.display = (main.style.display == 'none' ? '' : 'none');
+    birdsection.style.display = (whalesection.style.display == 'block' ? '' : 'block');
+}
+
+function switchBirdWhale(){
+    birdsection.style.display = (main.style.display == 'none' ? '' : 'none');
+    whalesection.style.display = (whalesection.style.display == 'block' ? '' : 'block');
+}
+
+function switchBirdLandscape(){
+    birdsection.style.display = (main.style.display == 'none' ? '' : 'none');
+    landscapesection.style.display = (whalesection.style.display == 'block' ? '' : 'block');
+}
