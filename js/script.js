1d3894bf7bca7935078119016dbf3925293c097c
Hifi58 <gillet.tony@gmail.com>
2021-09-13 13:53:31 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let main = document.getElementById('main');
+let whalesection = document.getElementById('whale-section');
+let whale = document.getElementById('whale');
+
+whale.addEventListener('click', function(){
+     main.style.display="none";
+     whalesection.style.display="block";
+})
